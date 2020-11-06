55ee7c999757fe394bccf0f0902ce96cbbb2c4f9
Sebastian Zagrodzki <sebek@zagrodzki.net>
2020-11-06 10:33:00 +0100

    do not try to upgrade msys2, that's impossible. Instead try to realize the truth, there is no need to upgrade...

--- .appveyor/upgrade_pacman.sh
+++ .appveyor/upgrade_pacman.sh
@@ -5,4 +5,3 @@ curl -O https://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.t
 pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig &&\
 pacman --noconfirm -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
 pacman --noconfirm -Syy pacman
-pacman --noconfirm -Su
