17fc0e6cca230e5d70e3cd58c2d9d2d7ee87049f
Sebastian Zagrodzki <sebek@zagrodzki.net>
2020-11-06 10:23:10 +0100

    set path

--- .appveyor/upgrade_pacman.sh
+++ .appveyor/upgrade_pacman.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export PATH="/mingw64/bin:${PATH}"
 curl -O https://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
 curl -O https://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig
 pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig &&\
