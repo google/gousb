f1b4ed7f0466a50b22e9aac27606b156b34763f3
Sebastian Zagrodzki <sebek@zagrodzki.net>
2020-11-06 10:36:19 +0100

    remove debugging find

--- .appveyor/install.sh
+++ .appveyor/install.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-set -x
 export PATH="/mingw64/bin:${PATH}"
 export PKG_CONFIG_PATH="/mingw64/lib/pkgconfig:${PKG_CONFIG_PATH}"
 export GOROOT=/mingw64/lib/go
 export GOPATH=/go
 export CGO_ENABLED=1
 
+set -x
+
 pacman --noconfirm -S \
     mingw-w64-x86_64-go \
     mingw-w64-x86_64-libusb
-
-find /var/cache/pacman
 
 go version
 go get -t github.com/google/gousb/...
