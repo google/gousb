0feeff1c892ab4a3728aa947fb08d385ed8c3fd4
Sebastian Zagrodzki <sebek@zagrodzki.net>
2021-01-07 19:33:56 +0100

    remove explicit installation of zstd and the test ls

--- .appveyor/upgrade_pacman.sh
+++ .appveyor/upgrade_pacman.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-ls -l /
 export PATH="/mingw64/bin:${PATH}"
 set -x
 curl -O https://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
@@ -7,4 +6,3 @@ curl -O https://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.t
 pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig &&\
 pacman --noconfirm -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
 pacman --noconfirm -Syy pacman
-pacman --noconfirm -Syy zstd
