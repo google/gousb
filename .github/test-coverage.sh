74689ff99eaf0a69311b880ce037b0372b13675a
Sebastian Zagrodzki <sebek@zagrodzki.net>
2021-11-01 15:04:31 +0100

    Use a github action for goveralls

--- .github/test-coverage.sh
+++ .github/test-coverage.sh
@@ -2,6 +2,3 @@
 echo 'mode: count' > coverage.merged &&\
         go list ./... | xargs -n1 -I'{}' sh -c ': > coverage.tmp; go test -v -covermode=count -coverprofile=coverage.tmp {} && tail -n +2 coverage.tmp >> coverage.merged' &&\
         rm coverage.tmp
-
-$HOME/go/bin/goveralls -coverprofile=coverage.merged -service=github -ignore libusb.go,error.go ||\
-        true
