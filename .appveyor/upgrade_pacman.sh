d3bdbe53c8b543b58a569a2d2bca4ccdcfbe9cd4
Sebastian Zagrodzki <sebek@zagrodzki.net>
2021-11-02 09:18:09 +0100

    Migrate from Travis/Appveyor combo to GitHub actions (#104)
    * Add a github actions workflow config (linux + windows build)
    * Remove Travis config, migrated to Github Actions
    * Remove AppVeyor configs, migrated to Github Actions

--- .appveyor/upgrade_pacman.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export PATH="/mingw64/bin:${PATH}"
-set -x
-curl -O https://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
-curl -O https://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig
-pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig &&\
-pacman --noconfirm -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
-pacman --noconfirm -Syy pacman
