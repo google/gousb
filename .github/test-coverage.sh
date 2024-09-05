6f7b3234e5bc75b24272b5f8a539c2d2ebf65397
blacknode <dev.blacknode@gmail.com>
2024-09-04 22:24:03 -0500

    se elimina codigo muerto

--- .github/test-coverage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo 'mode: count' > coverage.merged &&\
-        go list -f '{{.Dir}}' ./... | xargs -I'{}' sh -c ': > coverage.tmp; go test -v -covermode=count -coverprofile=coverage.tmp {} && tail -n +2 coverage.tmp >> coverage.merged' &&\
-        rm coverage.tmp
